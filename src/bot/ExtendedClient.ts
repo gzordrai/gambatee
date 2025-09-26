dcad9c3eb4f54c640a902aa2668a09b8c5c84f51
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-09-26 20:33:03 +0200

    chore: init cargo

--- src/bot/ExtendedClient.ts
+++
@@ -1,8 +0,0 @@
-import { Client, Collection } from "discord.js";
-import { Command } from "./Command";
-import { Prediction } from "../prediction";
-
-export class ExtendedClient extends Client {
-    public commands: Collection<string, Command> = new Collection<string, Command>();
-    public predictions: Collection<string, Prediction> = new Collection<string, Prediction>();
-}
--- src/prediction/Choices.ts
+++
@@ -1,7 +0,0 @@
-export interface Choice {
-    id: number;
-    name: string;
-    odds: number;
-}
-
-export type Choices = Array<Choice>;
