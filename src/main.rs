43c223d94273038319e1925bfec47ad2a2444565
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-09-28 02:02:05 +0200

    feat: setup bot

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
+use dotenv::dotenv;
+use serenity::{async_trait, Client};
+use serenity::all::{Context, EventHandler, GatewayIntents, Ready};
+
 mod rarety;
 
-fn main() {
-    println!("Hello, world!");
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("Logged in as {}", ready.user.tag())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    
+    let token = std::env::var("TOKEN").expect("Failed to find token");
+    let intents = GatewayIntents::GUILD_MESSAGES;
+    let mut client = Client::builder(&token, intents).event_handler(Handler).await.expect("Error creating client");
+
+    if let Err(e) = client.start().await {
+        println!("Client error: {e:?}");
+    }
 }
