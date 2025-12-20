448f11ead7d30ef95799d3cfcec394f83234b598
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-12-20 18:22:22 +0100

    fix: user stats are now correctly counted

--- src/handlers/voice_state_update.rs
+++ src/handlers/voice_state_update.rs
@@ -168,14 +168,13 @@ impl<'a> VoiceStateTransition<'a> {
         if channel_is_empty(self.ctx, &guild_channel).await? {
             info!("Channel {} is empty, deleting it", channel_id);
             channel_id.delete(&self.ctx).await?;
-
-            if let Some(member) = &old_state.member {
-                self.stats.user_left(&member.user).await?;
-            }
-
             debug!("Successfully deleted channel {}", channel_id);
         } else {
             debug!("Channel {} still has members, keeping it", channel_id);
+        }
+
+        if let Some(member) = &old_state.member {
+            self.stats.user_left(&member.user).await?;
         }
 
         Ok(())
